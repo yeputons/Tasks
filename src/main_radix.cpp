c41c1956b9a0c2fe8a112e0bb2d1feccef7ca302
Egor Suvorov <egor.suvorov@gmail.com>
2018-10-29 20:33:51 +0000

    Reduce n for radix/bitonic tests

--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 2 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
